5d6db00bbc021b52d8a0df707cd273787e8bb266
Jahidun nur <differentmahee13@gmail.com>
2021-02-25 12:26:57 +0600

    adding new thi9ngs

--- src/App.js
+++ src/App.js
@@ -13,26 +13,41 @@ function App() {
   }
   // destructuring the object
   const {name,job}=person ;
+
+  const arrayOfName = ['Jahidun','Mahee','Eraa','Shohaib','Ashik','Suzon'];
+  const singleName =arrayOfName.map(name=>name);
+  console.log(singleName);
+
   return (
     
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          {
+            arrayOfName.map(name=> <li>{name}</li>)
+          }
+        </ul>
         <p className="" style={style}>My Name is {name}</p>
         <p style={{backgroundColor:'gold',color:'black'}}>My job is {job}</p>
         <p>My first react lesson {98-89}</p>
-        <Myinfo></Myinfo>
+        <Myinfo name={arrayOfName[0]} job="programmer"></Myinfo>
+        <Myinfo name={arrayOfName[1]} job="Super women"></Myinfo>
+        
       </header>
       
     </div>
     
   );
-  function Myinfo(){
-    return <h4 style={{padding:'10px',border:'2px solid white'}}>Hello world I am Jahidun Nur Mahee and currently i am learning React!</h4>
-  }
+  
+}
+function Myinfo(props){
+  return (
+  <div style={{border:'2px solid white',width:'450px',margin:'10px',padding:'15px'}}> 
+  <h4 style={{padding:'10px',border:'2px solid white'}}>{props.name}</h4>
+  <h5>{props.name+" "+ props.job}</h5>
+  </div>
+  )
 }
 
 export default App;
